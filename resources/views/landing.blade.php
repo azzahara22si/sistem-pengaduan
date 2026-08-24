877216ce8cc29152cfa4ef6f15ff6595b3d0e456
Azzahara Tunnisyah <azzahara22si@mahasiswa.pcr.ac.id>
2026-08-24 21:44:37 +0700

    revisi terakhir bismillah

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -8,6 +8,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="{{ asset('css/responsive.css') }}">
 
     <style>
     :root {
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,7 @@
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="{{ asset('css/responsive.css') }}">
 
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
